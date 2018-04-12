ff841ed29a66c4f71ee6178110ab0069e20f9a60
jaimeabbariao <jaimeabbariao@gmail.com>
2018-04-11 20:40:12 -0400

    refactored redux code

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,30 @@ import ReactDOM from 'react-dom'
 import {AppContainer} from 'react-hot-loader'
 
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+
+const store = configureStore();
+
+console.log(store.getState());
+
+store.dispatch(addExpense({
+  description: 'water bill',
+  amount: 350
+}));
+
+store.dispatch(addExpense({
+  description: 'gas bill',
+  amount: 1200
+}));
+
+store.dispatch(setTextFilter('water'));
+
+const state = store.getState();
+
+console.log(getVisibleExpenses(state.expenses, state.filters));
 
 const renderApp = (Component) => {
   ReactDOM.render(
--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,7 @@ module.exports = {
     'core-js/fn/promise',
     'core-js/es6/object',
     'core-js/es6/array',
-    './src/playgrounds/redux-expensify.js'
-    //'./src/index.jsx', // your app's entry point
+    './src/index.jsx', // your app's entry point
   ],
   devtool: process.env.WEBPACK_DEVTOOL || 'eval-source-map',
   output: {
